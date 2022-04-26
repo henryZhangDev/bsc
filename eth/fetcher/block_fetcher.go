2a4c024f5025cfec4d145d9b34284136b37ec744
henryZhang <zhanghaishen@pin-dao.cn>
2022-04-26 22:21:57 +0800

    enlarge timeout

--- eth/fetcher/block_fetcher.go
+++ eth/fetcher/block_fetcher.go
@@ -34,8 +34,8 @@ import (
 
 const (
 	lightTimeout        = time.Millisecond       // Time allowance before an announced header is explicitly requested
-	arriveTimeout       = 50 * time.Millisecond // Time allowance before an announced block/transaction is explicitly requested
-	gatherSlack         = 10 * time.Millisecond // Interval used to collate almost-expired announces with fetches
+	arriveTimeout       = 150 * time.Millisecond // Time allowance before an announced block/transaction is explicitly requested
+	gatherSlack         = 25 * time.Millisecond // Interval used to collate almost-expired announces with fetches
 	fetchTimeout        = 5 * time.Second        // Maximum allotted time to return an explicitly requested block/transaction
 	reQueueBlockTimeout = 500 * time.Millisecond // Time allowance before blocks are requeued for import
 
